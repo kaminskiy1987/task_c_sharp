927b1bedf8571785d09658c9229ccf10599b89fb
Mikhail <m.kaminskiy@list.ru>
2022-06-28 19:55:16 +0700

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Задача по нахождению второй цифры в трехзначном числе");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите трехзначное число");
+int tripleDidits = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("------------------------------");
+
+int secondDigitInNumber = (tripleDidits % 100 - tripleDidits % 10 ) / 10;
+Console.WriteLine("Вторая цифра в числе: " + secondDigitInNumber);
