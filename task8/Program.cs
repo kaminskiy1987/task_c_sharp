c3914c292f5926d9dab1f677d83a3175ed8175b2
Mikhail <m.kaminskiy@list.ru>
2022-06-28 19:49:01 +0700

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача по нахождению четных чисел");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите число");
+int endRange = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("------------------------------");
+
+int startRange = 1;
+int i = startRange;
+
+while(i <= endRange)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write( " " + i);
+    }
+
+    i++;
+}
