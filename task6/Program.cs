e4f51f888a806b3536cb18ab74199d2338ec933c
Mikhail <m.kaminskiy@list.ru>
2022-06-28 19:46:28 +0700

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Задача по определению четности числа");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите число");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+if (digit1 % 2 == 0)
+{
+    Console.WriteLine(digit1 + " - четное число");
+}
+else
+{
+    Console.WriteLine(digit1 + " - нечетное число");
+}
