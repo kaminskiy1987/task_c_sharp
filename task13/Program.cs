a47e9a71854a328cc3ca6b71ad3406cf32590ce9
Mikhail <m.kaminskiy@list.ru>
2022-06-28 19:58:50 +0700

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача по нахождению третьей цифры в числе");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("------------------------------");
+
+int thirdDiditInNumber = 0;
+
+if (digit > 99)
+{
+    thirdDiditInNumber = (digit / 100) % 10; 
+    Console.Write( "Третья цифра в числе: " + thirdDiditInNumber); 
+}
+else 
+{
+    Console.Write( "Третьей цифры нет"); 
+}
