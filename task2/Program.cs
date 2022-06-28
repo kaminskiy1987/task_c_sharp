17a256c8f7f6b60fe8e4624ec662f60fb0d9debe
Mikhail <m.kaminskiy@list.ru>
2022-06-28 19:35:07 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Задача по сравнению двух чисел");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите первое число");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+bool compare1 = digit1 > digit2;
+bool compare2 = digit1 == digit2;
+bool compare3 = digit1 < digit2;
+
+if (compare1)
+{
+    Console.WriteLine("Большее число = " + digit1 + "; Меньшее число = " + digit2);
+}
+else if(compare2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine("Большее число = " + digit2 + "; Меньшее число = " + digit1);
+}
