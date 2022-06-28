c70e5a796de83ddf91f056f5566247b8a330688c
Mikhail <m.kaminskiy@list.ru>
2022-06-28 20:02:07 +0700

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача по определению выходного дня");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit > 7 || digit == 0)
+{
+    Console.WriteLine("Неправильный ввод");
+}
+else if(digit < 6)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной день");
+}
