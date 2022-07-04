016bc71e30d91bfa6712b49b0230866704c90c01
Mikhail <m.kaminskiy@list.ru>
2022-07-04 18:15:03 +0700

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = getData("Введите число: ");
+int startRange = 1;
+int result = 0;
+
+while(startRange <= number)
+{
+    result = getCube(startRange);
+
+    startRange++;
+
+    Console.Write(result + " ");
+  
+}
+
+int getData(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int getCube(int number)
+{
+return number * number * number;
+}
