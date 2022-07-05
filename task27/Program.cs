f524f08ca3b0ed1f9cd24b5efd00b5a2a44f1823
Mikhail <m.kaminskiy@list.ru>
2022-07-05 20:49:40 +0700

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = getData("Введите число : ");
+
+int sum = 0;
+
+while(number != 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine("Сумма чисел = " + sum);
+
+int getData(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
