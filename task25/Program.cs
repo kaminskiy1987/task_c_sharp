f805798b4b321dc2dad1ea296647affaf777ee0f
Mikhail <m.kaminskiy@list.ru>
2022-07-05 20:49:24 +0700

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = getData("Введите число : ");
+int level = getData("Введите степень : ");
+int result = 1;
+
+for(int i = 0; i < level; i++)
+{
+    result *= number;
+}
+
+Console.WriteLine("{0} ^ {1} = {2}", number, level, result );
+
+int getData(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
