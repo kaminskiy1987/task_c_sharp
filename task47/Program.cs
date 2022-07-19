ee9bc05fd307b4a785e2d614e27ed634cbc2a2aa
Mikhail <m.kaminskiy@list.ru>
2022-07-19 20:47:56 +0700

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,36 @@
+﻿int rows = getData("Введите количество строк: ");
+int columns = getData("Введите количество столбцов: ");
+double[,] numbers = new double[rows, columns];
+
+fillMatrix(numbers);
+printMatrix(numbers);
+
+void printMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void fillMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            numbers[i, j] = new Random().Next(-10, 10) + new Random().NextDouble();
+        }
+    }
+}
+
+int getData(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
