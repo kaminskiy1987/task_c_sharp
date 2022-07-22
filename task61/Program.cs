c0bb8dbdd1e48022abc767ca13b874dc651880e6
Mikhail <m.kaminskiy@list.ru>
2022-07-22 17:08:36 +0700

    task61

---
+++ task61/Program.cs
@@ -0,0 +1,37 @@
+﻿int count = getData("Введите количество строк : ");
+int i = 0;
+int j = 0;
+
+for( i = 0; i < count; i++)
+{
+        
+    for( j = 0; j < (count - i); j++)
+    {
+         Console.Write(" ");
+    }
+
+    for( j = 0; j <= i; j++)
+    {
+         Console.Write(" ");
+         Console.Write(factorial(i) / (factorial(j) * factorial(i - j)));
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+ }
+
+int factorial(int number)
+{
+    int factorial = 1;
+
+    for(int i = 1; i <= number; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int getData(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
