a83cc9d38cee06f7f5b4c2faa28027c5881fa382
Mikhail <m.kaminskiy@list.ru>
2022-07-25 20:50:34 +0700

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Сумма чисел в диапазоне : " + getSumNaturalNumber(m, n));
+
+
+int getSumNaturalNumber(int m, int n)
+{
+    int max = n;
+    int min = m;
+
+    if (max < min)
+    {
+        max = m;
+        min = n;
+    }
+
+    if (min == max) return min;
+    else
+    {
+        return max + getSumNaturalNumber(max - 1, min);
+    }
+}
