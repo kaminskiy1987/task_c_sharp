3dda77df3ff440e894399aefc8d8d756e4984627
Mikhail <m.kaminskiy@list.ru>
2022-07-25 20:50:46 +0700

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Функция Аккермана равна: " + calculateFunction(m, n));
+
+
+int calculateFunction(int m, int n)
+{
+    if( m == 0) return n + 1;
+    if( n == 0 && m != 0) return calculateFunction(m - 1, 1);
+    return calculateFunction( m - 1,calculateFunction(m, n - 1 )); 
+}
