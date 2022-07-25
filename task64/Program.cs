7107a9c126862af793fd300d713586b30193a5c5
Mikhail <m.kaminskiy@list.ru>
2022-07-25 20:50:19 +0700

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.Write("\"");
+Console.Write("\"");
+Console.Write(getRangeNaturalNumber(n, m));
+Console.Write("\"");
+Console.Write("\"");
+
+int getRangeNaturalNumber(int m, int n)
+{
+    int max = n;
+    int min = m;
+
+    if (max < min)
+    {
+        max = m;
+        min = n;
+    }
+
+    if (min == max) return min;
+    else
+    {
+        Console.Write(getRangeNaturalNumber(max - 1, min) + ", ");
+        return max;
+    }
+}
